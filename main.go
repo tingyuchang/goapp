43649ee34049d1b9c2144562f5b19ddbad570586
tingyuchang <tingyugenius@gmail.com>
2021-09-25 08:57:45 +0800

    using args to define expose port

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net/http"
 )
 
 func main()  {
+	var port string
+	flag.StringVar(&port, "port", "8080", "http port")
+	flag.Parse()
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+		io.WriteString(w, "Hello, this is port "+port+"\n")
 	}
 
 	http.HandleFunc("/", helloHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 
